e32a4d3a5d34e1881cd8546bd0b2822ad677ecf5
Lakshay Aggarwal <lakshayaggarwal36@gmail.com>
2025-10-17 21:53:21 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup( 
+    name='srapper',
+    version='0.0.1',
+    author='pwskills',
+    author_email='lakshayaggarwal36@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+)
